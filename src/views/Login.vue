71af4cb9a1f69aae5ff4df2920b1c6587483e174
wangzhongbin <wangzb@163.com>
2022-06-09 09:40:42 +0800

    u

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -36,8 +36,8 @@ export default {
   data () {
     return {
       uuid: '',
-      userName: 'admin',
-      password: 'admin123',
+      userName: '',
+      password: '',
       code: '',
       codeImgae: '',
       logo
